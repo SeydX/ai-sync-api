e219507485fcba0a863edc217557cd7d8911d339
SeydX <seyd55@outlook.de>
2021-10-04 11:25:58 +0200

    v1.0.0-beta.0

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var format = require('util').format;
-var request = require('request').defaults({jar: true/*, proxy:"http://localhost:8888", strictSSL:false*/}); // use cookies
-var WebSocket = require('ws')
-var events = require('events');
-
+const debug = require('debug')('aisync:api');
+const events = require('events');
+const request = require('request').defaults({ jar: true /*, proxy:"http://localhost:8888", strictSSL:false*/ }); // use cookies
+const WebSocket = require('ws');
 
 module.exports = {
-  AISyncApi: AISyncApi
-}
+  AISyncApi: AISyncApi,
+};
 
 function AISyncApi(config) {
   this.email = config.email;
@@ -14,7 +13,6 @@ function AISyncApi(config) {
   this.eventEmitter = new events.EventEmitter();
 
   this.baseURL = 'https://aisync.apps.exosite.io/api:1';
-  
 
   // interested parties in us being logged in
   this._loginCompleteCallbacks = [];
@@ -24,184 +22,222 @@ function AISyncApi(config) {
   this.messageID = 1;
 
   this.connection = null;
-
+  this.openedSocket = null;
+  this.timeInterval = 10000;
 }
 
 // /**
 //  * Login Process
 //  */
 
- AISyncApi.prototype.login = function(callback) {
-   // queue this callback for when we're finished logging in
-   if (callback) {
+AISyncApi.prototype.login = function (callback) {
+  // queue this callback for when we're finished logging in
+  if (callback) {
     this._loginCompleteCallbacks.push(callback);
-   }
-     
-   // begin logging in if we're not already doing so
-   if (!this._loggingIn) {
-     this._loggingIn = true;
-     this._beginLogin();
-   }
- }
-
- AISyncApi.prototype._loginComplete = function(err) {
-   
-   var self = this;
-   const url = this.baseURL + '/phone';
-   this.connection = new WebSocket(url);
-
-   this.connection.onmessage = (e) => {
-     if(e !== undefined) {
-      var data = JSON.parse(e.data);
-      // console.log(data.id);
-      if(this.responders[data.id] !== undefined) {
-        this.responders[data.id](data);
-        delete this.responders[data.id];
-      }
-      else {
-        if(data.event == 'device_change') {
-          self.eventEmitter.emit("device_change", data);
-        } else {
-          console.log("Unknown event type:");
-          console.log(data);
-        }
-        
-      }
-     }
-    
   }
+
+  // begin logging in if we're not already doing so
+  if (!this._loggingIn) {
+    this._loggingIn = true;
+    this._beginLogin(); //aka reconnect
+
+    setInterval(() => {
+      if (this.openedSocket == false) {
+        debug('Reconnecting...');
+        this._beginLogin();
+      }
+    }, this.timeInterval);
+  }
+};
+
+AISyncApi.prototype._loginComplete = function (err) {
+  return new Promise((resolve, reject) => {
+    const url = this.baseURL + '/phone';
+    this.connection = new WebSocket(url);
 
     this.connection.onopen = () => {
-    
-      setTimeout(function() {
-        self.WSsend({
-          "request":"login",
-          "data":
-            {
-              "token": self.token
-            }
-        }, function(data) {
-          self._loggedIn = true;
-          self._loginCompleteCallbacks.forEach(function(callback) { callback(err); });
-          self._loginCompleteCallbacks = [];
-        });
-      }, 500);
-      
-    }
-    
-    this.connection.onerror = (error) => {
-      this._socketOpen = false;
-      //todo reopen socket?
-      console.log(`WebSocket error: ${error}`)
-    }
-    
-    
- }
+      this.openedSocket = true;
 
-AISyncApi.prototype.WSsend = function(json, callbackMethod) {
-  var msgID = this.messageID++;
+      setTimeout(() => {
+        this.WSsend(
+          {
+            request: 'login',
+            data: {
+              token: this.token,
+            },
+          },
+          // eslint-disable-next-line no-unused-vars
+          (data) => {
+            this._loggedIn = true;
+            this._loginCompleteCallbacks.forEach((callback) => {
+              callback(err);
+            });
+            this._loginCompleteCallbacks = [];
+          }
+        );
+      }, 500);
+
+      resolve(this.openedSocket);
+    };
+
+    this.connection.onmessage = (e) => {
+      if (e !== undefined) {
+        const data = JSON.parse(e.data);
+        debug(data);
+        // console.log(data.id);
+        if (this.responders[data.id] !== undefined) {
+          this.responders[data.id](data);
+          delete this.responders[data.id];
+        } else {
+          if (data.event == 'device_change') {
+            this.eventEmitter.emit('device_change', data);
+          } else {
+            debug('Unknown event type:');
+            debug(data);
+          }
+        }
+      }
+    };
+
+    this.connection.onclose = (err) => {
+      this.openedSocket = false;
+      debug('WEBSOCKET_CLOSE: connection closed %o', err);
+
+      reject(err);
+    };
+
+    this.connection.onerror = (err) => {
+      this.openedSocket = false;
+      debug('WEBSOCKET_ERROR: Error', new Error(err.message));
+
+      reject(err);
+    };
+  });
+};
+
+AISyncApi.prototype.WSsend = function (json, callbackMethod) {
+  const msgID = this.messageID++;
   json.id = msgID;
   this.responders[msgID] = callbackMethod;
-  this.connection.send(
-    JSON.stringify(json)
-  );
-}
+  this.connection.send(JSON.stringify(json));
+};
 
-AISyncApi.prototype.wslog = function(data) {
-  console.log(data);
-}
+AISyncApi.prototype.wslog = function (data) {
+  debug(data);
+};
 
-AISyncApi.prototype._beginLogin = function() { 
+AISyncApi.prototype._beginLogin = function () {
+  const url = this.baseURL + '/session';
 
-  var url = this.baseURL + "/session";
-
-  var self = this;
-
-  request.post(url, {
+  request.post(
+    url,
+    {
       json: {
-        "email": self.email,
-        "password": self.password
+        email: this.email,
+        password: this.password,
+      },
+    },
+    async (err, response, body) => {
+      if (response.statusCode === 200) {
+        this.token = body.token;
+
+        try {
+          await this._loginComplete(); //aka connect
+        } catch (err) {
+          debug('WEBSOCKET_RECONNECT: Error', new Error(err.message));
+        }
+      } else {
+        debug('Got an error');
+        debug(body);
       }
-    }, function (err, response, body) {
-
-    if(response.statusCode === 200) {
-      this.token = body.token;
-
-      this._loginComplete();
-
-    } else {
-      console.log("Got an error");
-      console.log(body);
     }
+  );
+};
 
+AISyncApi.prototype.getDevices = function (callback) {
+  if (!this._loggedIn) {
+    this.login((err) => {
+      if (err) {
+        return callback(null, err);
+      }
 
-  }.bind(this));
-}
-
-AISyncApi.prototype.getDevices = function(callback) {
-
-  if(!this._loggedIn) {
-    this.login(function(err) {
-      if (err) return callback(null, err);
       this.getDevices(callback);
-    }.bind(this));
+    });
     return;
   }
 
-  this.WSsend({
-    "request": "lst_device"
-  }, function(data) {
-    callback(data);
-  });
+  this.WSsend(
+    {
+      request: 'lst_device',
+    },
+    (data) => {
+      callback(data);
+    }
+  );
+};
 
-}
+AISyncApi.prototype.deviceStatus = function (device, callback) {
+  if (!this._loggedIn) {
+    this.login((err) => {
+      if (err) {
+        return callback(null, err);
+      }
 
-AISyncApi.prototype.deviceStatus = function(device, callback) {
-  if(!this._loggedIn) {
-    this.login(function(err) {
-      if (err) return callback(null, err);
       this.deviceStatus(callback);
-    }.bind(this));
+    });
     return;
   }
-  
-  this.WSsend({
-    "request": "get",
-    "device": device,
-    "data": null
-  }, function(data) {
-    callback(data);
-  })
 
+  this.WSsend(
+    {
+      request: 'get',
+      device: device,
+      data: null,
+    },
+    (data) => {
+      callback(data);
+    }
+  );
+};
 
-}
+AISyncApi.prototype.fanOnOff = function (device, value, callback) {
+  this.WSsend(
+    {
+      device: device,
+      request: 'set',
+      data: { H00: value },
+    },
+    // eslint-disable-next-line no-unused-vars
+    (data) => {
+      callback(null);
+    }
+  );
+};
 
-AISyncApi.prototype.fanOnOff = function(device, value, callback) {
-  this.WSsend({
-    "device": device,
-    "request": "set",
-    "data": {"H00": value}
-  }, function(data) {
-    callback(null);
-  })
-}
+AISyncApi.prototype.fanSpeed = function (device, value, callback) {
+  this.WSsend(
+    {
+      device: device,
+      request: 'set',
+      data: { H02: value },
+    },
+    // eslint-disable-next-line no-unused-vars
+    (data) => {
+      callback(null);
+    }
+  );
+};
 
-AISyncApi.prototype.fanSpeed = function(device, value, callback) {
-  this.WSsend({
-    "device": device,
-    "request": "set",
-    "data": {"H02": value}
-  }, function(data) {
-    callback(null);
-  })
-}
-
-AISyncApi.prototype.lightOnOff = function(device, value, callback) {
-  this.WSsend({
-    "device": device,
-    "request": "set",
-    "data": {"H0B": value}
-  }, function(data) {
-    callback(null);
-  })
-}
+AISyncApi.prototype.lightOnOff = function (device, value, callback) {
+  this.WSsend(
+    {
+      device: device,
+      request: 'set',
+      data: { H0B: value },
+    },
+    // eslint-disable-next-line no-unused-vars
+    (data) => {
+      callback(null);
+    }
+  );
+};
